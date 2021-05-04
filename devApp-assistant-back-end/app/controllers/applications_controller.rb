95b7c460cbaad5d87132f09f7b8b5c60613d75da
Wurdsmith <wurdsmithing@gmail.com>
2021-05-04 04:08:27 -0500

    Adds a show controller action to each model.

--- devApp-assistant-back-end/app/controllers/applications_controller.rb
+++ devApp-assistant-back-end/app/controllers/applications_controller.rb
@@ -3,4 +3,9 @@ class ApplicationsController < ApplicationController
     def index
         render(json: Application.all)
     end
+
+    def show
+        application = Application.find_by(id: params[:id])
+        render(json: application)
+    end
 end
--- devApp-assistant-back-end/app/controllers/companies_controller.rb
+++ devApp-assistant-back-end/app/controllers/companies_controller.rb
@@ -3,4 +3,9 @@ class CompaniesController < ApplicationController
     def index
         render(json: Company.all)
     end
+
+    def show
+        company = Company.find_by(id: params[:id])
+        render(json: company)
+    end
 end
