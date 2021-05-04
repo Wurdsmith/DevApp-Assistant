811630613bb66191cf135140f3cd1e66c1a3f1c9
Wurdsmith <wurdsmithing@gmail.com>
2021-05-04 01:42:17 -0500

    Sets up the initials back-end API. Seed data and controller files still need to be added for initial testing.

---
+++ devApp-assistant-back-end/db/migrate/20210504060438_create_applications.rb
@@ -0,0 +1,17 @@
+class CreateApplications < ActiveRecord::Migration[6.1]
+  def change
+    create_table :applications do |t|
+      t.string :job_title
+      t.string :application_date
+      t.string :status
+      t.string :notes
+      t.string :website_link
+      t.string :email_address
+      t.string :follow_up
+      t.string :date
+      t.belongs_to :company, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ devApp-assistant-back-end/db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_04_060438) do
+
+  create_table "applications", force: :cascade do |t|
+    t.string "job_title"
+    t.string "application_date"
+    t.string "status"
+    t.string "notes"
+    t.string "website_link"
+    t.string "email_address"
+    t.string "follow_up"
+    t.string "date"
+    t.integer "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_applications_on_company_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "applications", "companies"
+end
