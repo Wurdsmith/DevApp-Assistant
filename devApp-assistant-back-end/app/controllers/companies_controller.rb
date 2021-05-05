7b51bb216b39fa63ac43b28b9d41138f72fc485e
Wurdsmith <wurdsmithing@gmail.com>
2021-05-05 05:32:34 -0500

    Adds the a create controller action for the companies model.

--- devApp-assistant-back-end/app/controllers/companies_controller.rb
+++ devApp-assistant-back-end/app/controllers/companies_controller.rb
@@ -8,4 +8,16 @@ class CompaniesController < ApplicationController
         company = Company.find_by(id: params[:id])
         render(json: company)
     end
+
+
+    def create
+        company = Company.create(company_params)
+        render(json: company)
+    end
+
+    private
+
+    def company_params
+        params.require(:company).permit(:name)
+    end
 end
