4e4a48b521180292f3c74d99cc06686022adfa67
Wurdsmith <wurdsmithing@gmail.com>
2021-05-06 02:37:26 -0500

    Fixes an issue where only on ul appeared under each company div id that contained multiple applications.

--- devApp-assistant-back-end/db/seeds.rb
+++ devApp-assistant-back-end/db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Company.create(name:"Mozilla")
+Company.create(name:"Target")
 
-Application.create(job_title: "Junior Developer", application_date: "06/25/2020", status:"pending", notes: "Application sent on 06/25/2020.", website_link: "www.mozilla.org", email_address: "mozilla@mozilla.org", follow_up_date: "07/05/2021", company_id: 1) 
+Application.create(job_title: "Junior Developer", application_date: "06/25/2020", status:"pending", notes: "Application sent on 06/25/2020.", website_link: "www.mozilla.org", email_address: "mozilla@mozilla.org", follow_up_date: "07/05/2021", company_id: 1)
+Application.create(job_title: "Senior Developer", application_date: "06/30/2020", status:"pending", notes: "Application sent on 06/30/2020.", website_link: "www.mozilla.org", email_address: "mozilla@mozilla.org", follow_up_date: "07/10/2021", company_id: 1)
+Application.create(job_title: "Frontend Developer", application_date: "07/05/2020", status:"pending", notes: "Application sent on 07/05/2020.", website_link: "www.target.org", email_address: "target@target.org", follow_up_date: "07/15/2021", company_id: 2)  
